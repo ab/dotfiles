ea80e33d6fd8e38afdfff52065d207cd92da860f
Andy Brody <andy+wisp@abrody.com>
2009-11-05 14:00:34 -0500

    change update.py to do symlinks by default

--- update.py
+++ update.py
@@ -4,6 +4,7 @@ import sys
 from subprocess import call
 
 home = os.getenv('HOME')
+cwd = os.getenv('PWD')
 FILE_LIST = '_file_list'
 
 dir_map = {'$HOME':os.getenv('HOME')}
@@ -34,6 +35,9 @@ class Runner(object):
     def __init__(self, conf_map, verbose=True):
         self.config_map = conf_map
         self.verbose = verbose
+
+    def __str__(self):
+        print "Generic sync runner class with no action"
     
     def pre_run(self):
         pass
@@ -53,21 +57,36 @@ class Runner(object):
         self.post_run()
 
 class CopyRunner(Runner):
+    def __str__(self):
+        return "CopyRunner"
     def action(self, conf, dest):
         opts = '-v' if self.verbose else ''
         call(['cp', '-a', opts, conf, dest])
 
 class LinkRunner(Runner):
+    def __str__(self):
+        return "LinkRunner"
     def action(self, conf, dest):
         opts = '-v' if self.verbose else ''
-        call(['ln', opts, '-s', conf, dest])
+        call(['ln', opts, '-sf', os.path.join(cwd, conf), dest])
+
+class DeleteRunner(Runner):
+    def __str__(self):
+        return "DeleteRunner"
+    def action(self, conf, dest):
+        opts = '-v' if self.verbose else ''
+        call(['rm', opts, dest])
 
 class RsyncRunner(Runner):
+    def __str__(self):
+        return "RsyncRunner"
     def action(self, conf, dest):
         v = 'v' if self.verbose else ''
         call(['rsync', '-ac' + v, conf, dest])
 
 class RsyncDryRunner(RsyncRunner):
+    def __str__(self):
+        return "RsyncDryRunner"
     def action(self, conf, dest):
         v = 'v' if self.verbose else ''
         call(['rsync', '-acn' + v, conf, dest])
@@ -78,14 +97,27 @@ if __name__ == '__main__':
     
     verbose = True
     runner = None
+    confirm = True
 
     if '-q' in sys.argv:
         verbose = False
     if '-n' in sys.argv:
         runner = RsyncDryRunner(filelist, verbose)
-
-    if not runner:
+        confirm = False
+    if '-y' in sys.argv:
+        confirm = False
+    
+    if 'rsync' in sys.argv and not runner:
         runner = RsyncRunner(filelist, verbose)
+    if 'rm' in sys.argv and not runner:
+        runner = DeleteRunner(filelist, verbose)
+    
+    if not runner:
+        runner = LinkRunner(filelist, verbose)
+
+    if confirm:
+        print "About to run " + str(runner) + ",",
+        raw_input("press enter to continue. ")
 
     runner.run()
 
